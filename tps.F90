1c9fd851f52bc8d7a847946d0368a1b11287692c
Weiqun Zhang <weiqunzhang@lbl.gov>
2018-01-29 12:43:04 -0800

    init fftw mpi

--- tps.F90
+++ tps.F90
@@ -6,7 +6,8 @@ module tps
 contains
 
   subroutine tps_mpi_init (comm_in) bind(c,name='tps_mpi_init')
-    use shared_data, only : comm, rank, nproc
+    use shared_data, only : comm, rank, nproc, fftw_threads_ok
+    use mpi_fftw3, only : fftw_mpi_init
     integer, value, intent(in) :: comm_in
     integer :: ierr, lnproc, lrank
     call mpi_comm_dup(comm_in, comm, ierr)
@@ -14,12 +15,18 @@ contains
     nproc = lnproc
     call mpi_comm_rank(comm, lrank, ierr)
     rank = lrank
+
+    ! fftw
+    fftw_threads_ok = .false.
+    call fftw_mpi_init()
   end subroutine tps_mpi_init
 
 
   subroutine tps_mpi_finalize () bind(c,name='tps_mpi_finalize')
     use shared_data, only : comm
+    use mpi_fftw3, only : fftw_mpi_cleanup
     integer :: ierr
+    call fftw_mpi_cleanup()
     call mpi_comm_free(comm, ierr)
   end subroutine tps_mpi_finalize
 
